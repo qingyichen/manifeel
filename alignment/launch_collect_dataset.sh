3ae8b2db7eb3088b992dbff4004ae3764dc0dc51
Qingyi Chen <chen5221@purdue.edu>
2026-06-16 15:09:34 -0700

    ADD: add launch script

--- alignment/launch_collect_dataset.sh
+++ alignment/launch_collect_dataset.sh
@@ -3,5 +3,6 @@ apptainer exec --nv --cleanenv --env LD_PRELOAD= manifeel.sif bash -ic "
   export LD_LIBRARY_PATH=/.singularity.d/libs:\${CONDA_PREFIX}/lib:\${LD_LIBRARY_PATH}
   python alignment/collect_dataset.py \
   -c data/outputs/tacff_usb_wrist_0805_50/0/checkpoints/latest_epoch999.ckpt \
-  --num_envs 4 --n_rounds 1 --max_steps 100
+  --num_envs 20 --n_rounds 5 --max_steps 400 \
+  --output alignment/data/usb.zarr
 "
