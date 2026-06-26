0b95122cfa08b29939f21f054052c7c30ee95123
Qingyi Chen <chen5221@purdue.edu>
2026-06-26 15:51:10 -0700

    ADD: update dataset collectino script

--- alignment/scripts/launch_collect_dataset.sh
+++ alignment/scripts/launch_collect_dataset.sh
@@ -1,7 +1,7 @@
 apptainer exec --nv --cleanenv --env LD_PRELOAD= manifeel.sif bash -ic "
   conda activate manifeel
   export LD_LIBRARY_PATH=/.singularity.d/libs:\${CONDA_PREFIX}/lib:\${LD_LIBRARY_PATH}
-  python alignment/collect_dataset.py \
+  python alignment/data_collection/collect_dataset.py \
   -c data/outputs/tacff_usb_wrist_0805_50/0/checkpoints/latest_epoch999.ckpt \
   --num_envs 20 --n_rounds 5 --max_steps 400 \
   --output alignment/data/usb.zarr
